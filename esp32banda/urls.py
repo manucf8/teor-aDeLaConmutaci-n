e3fd7c88796e4f519976cf3ac040a8fa18113fd4
santiagoneusa <ruizsantiago836@gmail.com>
2023-10-30 22:53:42 -0500

    Base files creation and eps32 app modification - .gitignore created because we're using a virtual env but don't want to upload that enviroment to github - requirementes.txt so the user who clones the project can install all the libraries required - creation of the view 'home' and templates folder

--- esp32banda/urls.py
+++ esp32banda/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from esp32bandap import views as espViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', espViews.home),
 ]
--- esp32bandap/views.py
+++ esp32bandap/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import serial
 
 # Create your views here.
+def home(request):
+
+    
+    
+    # ser = serial.Serial('COM3', 9600, timeout=1)
+    # ser.write(b"")
+
+    return render(request, 'home.html')
