ddcd8a7827b393d9a7c01d16906f75ec7303e3f8
santiagoneusa <ruizsantiago836@gmail.com>
2023-10-31 01:41:37 -0500

    modify parcial.ino - add global variables - use serial communication in void loop() - creation of move function

--- parcial/parcial.ino
+++ parcial/parcial.ino
@@ -1,10 +1,15 @@
-int s4 = 33;
-int s3 = 32;
-int s2 = 35;
 int s1 = 34;
-
+int s2 = 35;
+int s3 = 32;
+int s4 = 33;
 int der = 27;
 int izq = 26;
+
+int A, B, delayTime, option;
+
+
+void move(int target);
+
 
 void setup() {
 
@@ -21,25 +26,55 @@ void setup() {
 
 void loop() {
 
-  while(digitalRead(s1)==0){
+  if (Serial.available() > 0) {
+    
+    option = Serial.read();
 
-    digitalWrite(der,1);
+    if (option > 119) {
 
+      A = option/100;
+      move(A);
+      delay(2000);
+
+      B = (option/10) % 10;
+      move(B);
+      
+      delayTime = option % 10;
+      delay(delayTime);
+      move(A);
+
+    }
+
+    else {
+
+      A = option/10;
+      move(A);
+      delay(2000);
+      B = option % 10;
+      move(B);
+
+    }
+
+    
   }
 
-  digitalWrite(der,0);
+}
 
+void move(target) {
 
-  delay(2000);
-  
-  while(digitalRead(s4)==0){
+  int current = 0;
+  if (digitalRead(s1) == 1) current = 1;
+  if (digitalRead(s2) == 1) current = 2;
+  if (digitalRead(s3) == 1) current = 3;
+  if (digitalRead(s4) == 1) current = 4;
 
-    digitalWrite(izq,1);
-
+  if (current != target) {
+    if (current > target) {
+      while (digitalRead(target) != 1) digitalWrite(der, 1);
+    }
+    else {
+      while (digitalRead(target) != 1) digitalWrite(izq, 1);
+    }
   }
-
-  digitalWrite(izq,0);
-
-  delay(2000);
 
 }
