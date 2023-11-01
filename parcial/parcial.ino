142bb98a0ea24e0a5d746510fea1829a6c0a20fc
santiagoneusa <ruizsantiago836@gmail.com>
2023-11-01 09:43:20 -0500

    mi amor te amooooooo

--- parcial/parcial.ino
+++ parcial/parcial.ino
@@ -1,117 +1,144 @@
-int s1 = 34;
-int s2 = 35;
-int s3 = 32;
-int s4 = 33;
-int der = 27;
-int izq = 26;
+// int s1 = 34;
+// int s2 = 35;
+// int s3 = 32;
+// int s4 = 33;
+// int der = 26;
+// int izq = 27;
 
-int positions[4] = {34, 35, 32, 33};
+// int positions[4] = {34, 35, 32, 33};
 
-void move(int target);
+// void move(int target);
 
 
-void setup() {
+// void setup() {
 
-  pinMode(s4, INPUT);
-  pinMode(s3, INPUT);
-  pinMode(s2, INPUT);
-  pinMode(s1, INPUT);
-  pinMode(der, OUTPUT);
-  pinMode(izq, OUTPUT);
+//   pinMode(s4, INPUT);
+//   pinMode(s3, INPUT);
+//   pinMode(s2, INPUT);
+//   pinMode(s1, INPUT);
+//   pinMode(der, OUTPUT);
+//   pinMode(izq, OUTPUT);
 
-  Serial.begin(115200);
+//   Serial.begin(115200);
 
-}
+// }
 
-void loop() {
-  if (Serial.available() > 0) {
+// void loop() {
+//   if (Serial.available() > 0) {
 
-    int option = Serial.parseInt();
-    Serial.print("Option: ");
-    Serial.println(option);
-    delay(2000);
+//     int option = Serial.parseInt();
+//     Serial.print("Option: ");
+//     Serial.println(option);
+//     delay(2000);
 
-    if (option == 1) {
+//     if (option == 1) {
 
-      while (Serial.available() == 0) {}
-      int A = Serial.parseInt();
-      A = A/100;
-      Serial.print("Option A: ");
-      Serial.println(A);
-      // move(A);
-      Serial.println();
+//       while (Serial.available() == 0) {}
+//       int A = Serial.parseInt();
+//       A = A/100;
+//       Serial.print("Option A: ");
+//       Serial.println(A);
+//       // move(A);
+//       Serial.println();
 
-      while (Serial.available() == 0) {}
-      int B = (Serial.parseInt()/10) % 10;
-      Serial.print("Option B: ");
-      Serial.println(B);
-      // move(B);
-      Serial.println();
+//       while (Serial.available() == 0) {}
+//       int B = (Serial.parseInt()/10) % 10;
+//       Serial.print("Option B: ");
+//       Serial.println(B);
+//       // move(B);
+//       Serial.println();
     
-      delay(2000);
-      while (Serial.available() == 0) {}
-      int delayTime = Serial.parseInt() % 10;
-      delay(delayTime*1000);
-      Serial.print("Return A: ");
-      Serial.println(A);
-      // move(A);
-      Serial.println();
+//       delay(2000);
+//       while (Serial.available() == 0) {}
+//       int delayTime = Serial.parseInt() % 10;
+//       delay(delayTime*1000);
+//       Serial.print("Return A: ");
+//       Serial.println(A);
+//       // move(A);
+//       Serial.println();
 
-    }
+//     }
 
-    else {
+//     else {
 
-      while (Serial.available() == 0) {}
-      int A = Serial.parseInt()/10;
-      move(A);
-      delay(2000);
+//       while (Serial.available() == 0) {}
+//       int A = Serial.parseInt()/10;
+//       move(A);
+//       delay(2000);
 
-      while (Serial.available() == 0) {}
-      int B = Serial.parseInt() % 10;
-      move(B);
+//       while (Serial.available() == 0) {}
+//       int B = Serial.parseInt() % 10;
+//       move(B);
 
-    }
+//     }
 
-  }
+//   }
 
-}
+// }
 
-void move(int target) {
+// void move(int target) {
 
-  int current = 0;
+//   int current = 0;
 
-  if (digitalRead(s1) == 1) {
-    current = 1;
-  }
-  else if (digitalRead(s2) == 1) {
-    current = 2;
-  }
-  else if (digitalRead(s3) == 1) {
-    current = 3;
-  }
-  else if (digitalRead(s4) == 1) {
-    current = 4;
+//   if (digitalRead(s1) == 1) {
+//     current = 1;
+//   }
+//   else if (digitalRead(s2) == 1) {
+//     current = 2;
+//   }
+//   else if (digitalRead(s3) == 1) {
+//     current = 3;
+//   }
+//   else if (digitalRead(s4) == 1) {
+//     current = 4;
+//   }
+  
+
+//   int final_position = positions[target-1];
+
+//   Serial.print("Current: ");
+//   Serial.println(current);
+
+//   if (current != target && current != 0) {
+//     if (current > target) {
+//       while (digitalRead(final_position) == 0) {
+//         digitalWrite(der, 1);
+//       }
+//       digitalWrite(der, 0);
+//     }
+//     else {
+//       while (digitalRead(final_position) == 0) { 
+//         digitalWrite(izq, 1);
+//       }
+//       digitalWrite(izq, 0);
+//     }
+//   }
+
+// }
+
+#define STATUS_LED_PIN 2
+
+int x; 
+void setup() { 
+  pinMode(STATUS_LED_PIN, OUTPUT); 
+	Serial.begin(115200); 
+	Serial.setTimeout(1); 
+} 
+void loop() { 
+	while (!Serial.available());
+
+  String text = Serial.readString();
+
+  if (text[0] == '1') {
+    digitalWrite(STATUS_LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(STATUS_LED_PIN, LOW);
+    delay(1000);
   }
   
 
-  int final_position = positions[target-1];
+  x = text.toInt();
+  int result = x + 1;
+  Serial.println(result);
 
-  Serial.print("Current: ");
-  Serial.println(current);
-
-  if (current != target && current != 0) {
-    if (current > target) {
-      while (digitalRead(final_position) == 0) {
-        digitalWrite(der, 1);
-      }
-      digitalWrite(der, 0);
-    }
-    else {
-      while (digitalRead(final_position) == 0) { 
-        digitalWrite(izq, 1);
-      }
-      digitalWrite(izq, 0);
-    }
-  }
-
-}
+} 
--- prueba.py
+++ prueba.py
@@ -1,7 +1,14 @@
-import serial, time
-arduino = serial.Serial('COM3', 115200)
-time.sleep(2)
-arduino.write(b"134")
-String = arduino.readline()
-print(String)
-arduino.close()
+# Importing Libraries 
+import serial 
+import time 
+arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1) 
+def write_read(x): 
+    arduino.write(bytes(x, 'utf-8')) 
+    time.sleep(0.05) 
+    data = arduino.readline() 
+    return data 
+
+while True: 
+    num = input("Enter a number: ") # Taking input from user 
+    value = write_read(num).decode('utf-8')
+    print(value) # printing the value 
