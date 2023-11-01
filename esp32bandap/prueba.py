6cf04d7327cda2e1f2652e1bdfb561a5790c1475
santiagoneusa <ruizsantiago836@gmail.com>
2023-11-01 12:16:34 -0500

    happy

---
+++ esp32bandap/prueba.py
@@ -0,0 +1,14 @@
+# Importing Libraries 
+import serial 
+import time 
+arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1) 
+def write_read(x): 
+    arduino.write(bytes(x, 'utf-8')) 
+    time.sleep(0.05) 
+    data = arduino.readline() 
+    return data 
+
+def call(msg):
+    num = str(msg).strip() # Taking input from user 
+    value = write_read(num).decode('utf-8')
+    print(value) # printing the value 
