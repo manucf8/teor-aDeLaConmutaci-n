ff81ced8f1db025badc744a8eb3d0b562f599cb0
Manuela <mcastanof1@eafit.edu.co>
2023-11-01 09:57:53 -0500

    la ultima es la vencida

--- esp32bandap/views.py
+++ esp32bandap/views.py
@@ -3,9 +3,16 @@ import serial
 import time
 from .forms import TransportForm
 
+def write_read(x, ser): 
+    ser.write(bytes(x, 'utf-8')) 
+    time.sleep(0.05) 
+    data = ser.readline() 
+    return data 
+
 def home(request):
 
     if request.method == "POST":
+        
         form = TransportForm(request.POST)
 
         if form.is_valid():
@@ -15,43 +22,26 @@ def home(request):
                 return render(request, 'home.html', {'form': form, 'error':'Posición inicial y final deben ser distintas.'})
     
             try:
-                ser = serial.Serial('COM3', 115200)
+                ser = serial.Serial('COM3', 115200, timeout=.1)
                 time.sleep(2)
                 print("Conectado")
 
             except serial.SerialException as e:
                 print(f"Error al abrir el puerto COM: {e}")
                 return render(request, 'home.html', {'form': form, 'error':'No fue posible conectarse con el puerto.'})
-            
-            if 'initial_position' in request.POST and 'final_position' in request.POST and 'duration' in request.POST:
-                ser.write(b'1')
-            else:
-                ser.write(b'0')
-            
-            A = str(request.POST['initial_position'])[1]
-            if A == '1': ser.write(b'1')
-            elif A == '2': ser.write(b'2')
-            elif A == '3': ser.write(b'3')
-            elif A == '4': ser.write(b'4')
-            
-            B = str(request.POST['final_position'])[1]
-            if B == '1': ser.write(b'1')
-            elif B == '2': ser.write(b'2')
-            elif B == '3': ser.write(b'3')
-            elif B == '4': ser.write(b'4')
+                       
 
-            if 'duration' in request.POST:
-                delayTime = request.POST['final_position']
-                if delayTime == '0': ser.write(b'0')
-                elif delayTime == '1': ser.write(b'1')
-                elif delayTime == '2': ser.write(b'2')
-                elif delayTime == '3': ser.write(b'3')
-                elif delayTime == '4': ser.write(b'4')
-                elif delayTime == '5': ser.write(b'5')
-                elif delayTime == '6': ser.write(b'6')
-                elif delayTime == '7': ser.write(b'7')
-                elif delayTime == '8': ser.write(b'8')
-                elif delayTime == '9': ser.write(b'9')
+            message = str(request.POST['initial_position'])[1]
+            message += str(request.POST['final_position'])[1]
+            if 'duration' in request.POST: 
+                message += request.POST['duration']
+                message = "1" +message
+            else:
+                message = "0" +message
+                 
+            
+            value = write_read(message, ser).decode('utf-8')
+            print(value)
 
             if 'ser' in locals() and ser.is_open: ser.close()
 
@@ -61,18 +51,3 @@ def home(request):
         form = TransportForm()
 
     return render(request, 'home.html', {'form': form})
-
-# Importing Libraries 
-import serial 
-import time 
-arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1) 
-def write_read(x): 
-    arduino.write(bytes(x, 'utf-8')) 
-    time.sleep(0.05) 
-    data = arduino.readline() 
-    return data 
-
-while True: 
-    num = input("Enter a number: ") # Taking input from user 
-    value = write_read(num).decode('utf-8')
-    print(value) # printing the value 
